764ba499550318572a99ed4bb75cb0249b62b730
Jose Berrio <aldarisbm@gmail.com>
2023-10-31 22:14:12 -0400

    maybe jinja will work better than all of these things

--- functions/dates.py
+++ functions/dates.py
@@ -3,15 +3,13 @@ from datetime import datetime
 
 def get_weekday(day: int, month: int, year: int) -> str:
     """
-    Returns the weekday for the provided date
-
+    Returns the weekday for a given date.
     Args:
-        day (int): The day
-        month (int): The month
-        year (int): The year
-
+        day (int): The day. eg: 1, 20, 19
+        month (int): The month. eg: 12, 10, 5
+        year (int): The year. eg: 2021, 1993, 1987
     Returns:
-        str: The weekday of the date provided
+        str: The weekday of the date provided. eg: Monday, Tuesday, Friday, Saturday
     """
     date_dict = {
         0: 'Monday',
@@ -23,6 +21,3 @@ def get_weekday(day: int, month: int, year: int) -> str:
         6: 'Sunday',
     }
     return date_dict[datetime(day=day, month=month, year=year).weekday()]
-
-
-print(get_weekday.__doc__)

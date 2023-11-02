852c684b67dba3419c32b28c7e7ee34db274eb18
Jose Berrio <aldarisbm@gmail.com>
2023-11-02 07:33:51 -0400

    tooling

---
+++ functions/location.py
@@ -0,0 +1,23 @@
+from uszipcode import SearchEngine
+
+
+def get_zipcode(city: str, state: str) -> str:
+    """
+    Returns a zipcode for a given city and state.
+    Args:
+        city (str): City.
+        state (str): State in two-letter abbreviation.
+    Returns:
+        str: The zipcode as a string.
+
+    Examples:
+        >>> get_zipcode("New York", "NY")
+        '10001'
+        >>> get_zipcode("Los Angeles", "CA")
+        '90001'
+        >>> get_zipcode("Chicago", "IL")
+        '60601'
+    """
+    engine = SearchEngine()
+    res = engine.by_city_and_state(city, state)
+    return res
---
+++ functions/text.py
@@ -0,0 +1,19 @@
+def get_unicode_point(char: str) -> int:
+    """
+    Returns a Unicode point as an integer for a given character.
+    Args:
+        character (str): The character to lookup.
+    Returns:
+        int: The Unicode point as an integer.
+
+    Examples:
+        >>> get_unicode_point('A')
+        U+0041
+        >>> get_unicode_point('€')
+        U+20AC
+        >>> get_unicode_point('😃')
+        U+1F603
+    """
+    if len(char) != 1:
+        raise Exception('character must be a single character')
+    return f"U+{ord(char)}"

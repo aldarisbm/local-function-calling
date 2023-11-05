02e5eeb76690707489c0e9cf9d259d5d96eae582
Jose Berrio <aldarisbm@gmail.com>
2023-11-05 00:13:20 -0400

    we are adding some wandb logging

--- run.py
+++ run.py
@@ -3,24 +3,27 @@ import logging
 import os
 from json import JSONDecodeError
 
+import pandas as pd
 from jinja2 import Template
 from llama_cpp import Llama
 
+import wandb
 from function_map import fns_map
 from helpers import load_template, load_llm, get_inference_params
 from inference import inference
 
 
 def run():
+    project = os.getenv('PROJECT', 'local-function-calling')
     examples: list[str] = ['What is the unicode point of the letter R']
     model_name = os.getenv('MODEL_NAME', 'airoboros-m-7b-3.1.2.Q8_0.gguf')
 
     generations: list[dict] = []
+    inference_params: dict = get_inference_params()
+    logging.debug(f'inference params: {inference_params}')
     for example_query in examples:
         ptpl: Template = load_template('functions')
         query: str = ptpl.render(query=example_query)
-        inference_params: dict = get_inference_params()
-        logging.debug(f'inference params: {inference_params}')
         llm: Llama = load_llm(inference_params)
         res, raw_output, t = inference(llm, query)
         tok_s = raw_output["usage"]["completion_tokens"] / t
@@ -52,3 +55,13 @@ def run():
         logging.info(f'Response: {resp}')
         generation_tracker.update({"is_valid": True})
         generations.append(generation_tracker)
+
+    pred_table = wandb.Table(dataframe=pd.DataFrame(generations))
+
+    # create a config to save with the project run
+    config = dict(model_name=model_name, params=inference_params)
+
+    # create a wandb run
+    with wandb.init(project=project, job_type="inference", config=config):
+        wandb.log({"preds_table": pred_table})
+        wandb.finish(quiet=True)

9039f78dc48d2b6612563d098dfa76d17db5489c
Jose Berrio <aldarisbm@gmail.com>
2023-11-12 18:27:06 -0500

    abstraction

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 from dotenv import load_dotenv
 
-from evals import fail
+from evals import regular
 from helpers import get_load_params, get_inference_params, get_available_functions
 from run import run
 
@@ -20,4 +20,12 @@ project = os.getenv('PROJECT', 'local_function')
 model_name = os.getenv('MODEL_NAME', 'airoboros-m-7b-3.1.2.Q6_K.gguf')
 available_functions = get_available_functions()
 
-run(project, model_name, inference_params, load_params, iterations, fail.tests, available_functions)
+run(
+    project,
+    model_name,
+    inference_params,
+    load_params,
+    iterations,
+    regular.tests,
+    available_functions
+)
